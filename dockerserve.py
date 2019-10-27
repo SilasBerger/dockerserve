794d795c8610c8be3913a6c95424f86887500580
Silas Berger <silas.berger@outlook.com>
2019-10-27 15:03:29 +0100

    Print port when running status command.

--- dockerserve.py
+++ dockerserve.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import sys
+import re
 
 
 DEFAULT_PORT = "8080"
@@ -40,10 +41,12 @@ def stop():
 
 def status():
     container_name = _get_container_name()
-    containers = subprocess.check_output(["docker", "container", "ls"]).decode("utf-8").split("\n")
+    containers = subprocess.check_output(["docker", "container", "ls"]).decode("utf-8").strip().split("\n")
     for line in containers:
         if container_name in line:
-            print("running (" + container_name + ")\n")
+            ports_pattern = re.compile("\\d+\\.\\d+\\.\\d+\\.\\d+:(\\d+)->80/tcp")
+            port = re.search(ports_pattern, line).group(1)
+            print("running (port=" + port + ", container=" + container_name + ")\n")
             return
     print("not running\n")
 
