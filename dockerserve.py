8bd4b925452bdbc543665266d2481900217323b4
Silas Berger <silas.berger@outlook.com>
2019-10-27 14:06:18 +0100

    Add port, remove obsolete code.

--- dockerserve.py
+++ dockerserve.py
@@ -14,9 +14,7 @@ def main():
         print_help()
         return
     command = sys.argv[1]
-    if command == "init":
-        init()
-    elif command == "start":
+    if command == "start":
         start()
     elif command == "stop":
         stop()
@@ -25,40 +23,20 @@ def main():
     return 0
 
 
-def init():
-    container_name = None
-    while container_name is None:
-        new_name = input("Container name: ")
-        if container_name_pattern.match(new_name):
-            container_name = new_name
-        else:
-            print("container name must match [a-zA-Z0-9][a-zA-Z0-9_.-]*")
-    with open("dockerserve", "w") as dockerserve_file:
-        dockerserve_file.truncate()
-        dockerserve_file.write(container_name)
-
-
 def start():
     if len(sys.argv) < 3:
         print_help()
         return
     container_name = _get_container_name()
     pwd = subprocess.check_output(["pwd"]).decode("utf-8").strip()
-    subprocess.run(["docker", "container", "run", "--rm", "-d", "--name", container_name,
+    port = sys.argv[2]
+    subprocess.run(["docker", "container", "run", "--rm", "-d", "--name", container_name, "-p", (port + ":80"),
                     "-v", (pwd + "://usr/share/nginx/html:ro"), "nginx:1.17"])
 
 
 def stop():
     container_name = _get_container_name()
     subprocess.run(["docker", "container", "stop", container_name])
-
-
-def _read_dockerserve_file():
-    try:
-        with open("dockerserve", "r") as dockerserve_file:
-            return dockerserve_file.readlines()
-    except FileNotFoundError:
-        print("Error: dockerserve file missing - run 'dockerserve init'\n")
 
 
 def _get_container_name():
@@ -72,7 +50,6 @@ def _get_container_name():
 def print_help():
     print("Serve the present working directory in an Nginx Docker container.\n")
     print("Usage:")
-    print("\tdockerserve init: initialize this directory for dockerserve")
     print("\tdockerserve start <port>: serve this directory")
     print("\tdockerserve stop: stop serving this directory")
     print("\tdockerserve: print this help")
